281d9b249e2d7dc2253acb98bde5c661facd2990
luohao <luohao@workstation.local>
2026-06-02 19:25:16 +0800

    update: build

--- app/mobi-web/src/contexts/video-mobi-context.tsx
+++ app/mobi-web/src/contexts/video-mobi-context.tsx
@@ -1,6 +1,6 @@
 import { createContext, useContext } from "react";
-import type { ProductInfoResp } from "../common/dto/product";
 import type { UserAuthInfoResp } from "@lib/common/dto/user";
+import type { ProductInfoResp } from "@lib/common/dto/product";
 
 interface VideoMobiContextType {
   userInfo: UserAuthInfoResp | undefined;
