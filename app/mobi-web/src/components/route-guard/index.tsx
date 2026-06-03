58dbf56cd1e45d51c18cfbd3e70f3242b5f89d01
hao <fortunellh@126.com>
2026-06-04 00:15:16 +0800

    update: ui

--- app/mobi-web/src/components/route-guard/index.tsx
+++ app/mobi-web/src/components/route-guard/index.tsx
@@ -7,7 +7,7 @@ import { useAuthCode } from "@app/mobi-web/hooks/user";
 import type { UserAuthInfoResp } from "@lib/common/dto/user";
 import { useProductInfo } from "@app/mobi-web/hooks/product";
 import { VideoMobiContext } from "@app/mobi-web/contexts/video-mobi-context";
-import { ClockArrowRotateLeft, House, Person } from "@gravity-ui/icons";
+import { House, Person } from "@gravity-ui/icons";
 
 const bottomTabs = [
   { path: "/", label: "Home", icon: <House /> },
