fdd7191b3ed5a02868d2ee31d9cb6032c17f97a0
hao <fortunellh@126.com>
2026-06-13 02:55:40 +0800

    update: fb pixel

--- app/mobi-web/src/components/payment/paypal-button.tsx
+++ app/mobi-web/src/components/payment/paypal-button.tsx
@@ -5,96 +5,104 @@ import { SkuType } from "@lib/common/consts/sku";
 import type { SkuListItem } from "@lib/common/dto/sku";
 import { DISPATCH_ACTION, PayPalButtons, usePayPalScriptReducer, type PayPalButtonsComponentProps } from "@paypal/react-paypal-js";
 import { useEffect } from "react";
+import { useLocation, useSearchParams } from "react-router";
 
 const styles: PayPalButtonsComponentProps["style"] = {
-    tagline: false,
-    shape: "rect",
-    layout: "horizontal",
-    color: "white",
-    label: "paypal",
-    height: 52,
+  tagline: false,
+  shape: "rect",
+  layout: "horizontal",
+  color: "white",
+  label: "paypal",
+  height: 52,
 };
 
 export default function PayPalButton({ skuInfo }: { skuInfo: SkuListItem }) {
-    const { productInfo } = useVideoMobiContext();
+  const location = useLocation();
+  const [searchParams, _setSearchParams] = useSearchParams();
 
-    const { fetchUserOrderCreate } = useUserOrderCreate();
-    const { fetchUserOrderApprove } = useUserOrderApprove();
-    const { fetchUserOrderClose } = useUserOrderClose();
+  const { productInfo } = useVideoMobiContext();
 
-    const [{ options }, dispatch] = usePayPalScriptReducer();
+  const { fetchUserOrderCreate } = useUserOrderCreate();
+  const { fetchUserOrderApprove } = useUserOrderApprove();
+  const { fetchUserOrderClose } = useUserOrderClose();
 
-    const currency = productInfo?.currency || "USD";
+  const [{ options }, dispatch] = usePayPalScriptReducer();
 
-    useEffect(() => {
-        if (skuInfo.skuType === SkuType.Subscription) {
-            dispatch({
-                type: DISPATCH_ACTION.RESET_OPTIONS,
-                value: { ...options, currency: currency, vault: true, intent: "subscription" },
-            });
-        } else {
-            dispatch({
-                type: DISPATCH_ACTION.RESET_OPTIONS,
-                value: { ...options, currency: currency, vault: false, intent: "capture" },
-            });
-        }
-    }, [currency]);
+  const currency = productInfo?.currency || "USD";
 
-    const handlePaypalCreateOrder: PayPalButtonsComponentProps["createOrder"] = async () => {
-        const result = await fetchUserOrderCreate({
-            sku: skuInfo.bizId,
-            paymentChannel: PaymentChannel.Paypal,
-            paymentType: PaymentType.Card,
-        });
-        return result.paymentId;
-    }
-
-    const handlePaypalOnApprove: PayPalButtonsComponentProps["onApprove"] = async (data) => {
-        await fetchUserOrderApprove({
-            paymentChannel: PaymentChannel.Paypal,
-            paymentId: data.paymentID || "",
-            subscriptionNo: data.subscriptionID || "",
-            paymentType: (data as any).paymentSource,
-        });
-    }
-
-    const handlePaypalOnCancel: PayPalButtonsComponentProps["onCancel"] = async (data) => {
-        await fetchUserOrderClose({
-            paymentChannel: PaymentChannel.Paypal,
-            paymentId: data.orderID as string,
-        })
-    }
-
-    const handlePaypalCreateSubscription: PayPalButtonsComponentProps["createSubscription"] = async () => {
-        const result = await fetchUserOrderCreate({
-            sku: skuInfo.bizId,
-            paymentChannel: PaymentChannel.Paypal,
-            paymentType: PaymentType.Card,
-        });
-        return result.subscriptionNo;
-    }
-
+  useEffect(() => {
     if (skuInfo.skuType === SkuType.Subscription) {
-        return (
-            <div className="rounded-[16px] overflow-hidden [&_iframe]:!rounded-[16px]">
-                <PayPalButtons
-                    style={styles}
-                    createSubscription={handlePaypalCreateSubscription}
-                    onApprove={handlePaypalOnApprove}
-                    onCancel={handlePaypalOnCancel}
-                />
-            </div>
-        )
+      dispatch({
+        type: DISPATCH_ACTION.RESET_OPTIONS,
+        value: { ...options, currency: currency, vault: true, intent: "subscription" },
+      });
+    } else {
+      dispatch({
+        type: DISPATCH_ACTION.RESET_OPTIONS,
+        value: { ...options, currency: currency, vault: false, intent: "capture" },
+      });
     }
+  }, [currency]);
 
+  const handlePaypalCreateOrder: PayPalButtonsComponentProps["createOrder"] = async () => {
+    const result = await fetchUserOrderCreate({
+      sku: skuInfo.bizId,
+      paymentChannel: PaymentChannel.Paypal,
+      paymentType: PaymentType.Card,
+      fbPixelId: searchParams.get('pixel') || '',
+      reback: `${location.pathname}${location.search || ''}`,
+    });
+    return result.paymentId;
+  }
+
+  const handlePaypalOnApprove: PayPalButtonsComponentProps["onApprove"] = async (data) => {
+    await fetchUserOrderApprove({
+      paymentChannel: PaymentChannel.Paypal,
+      paymentId: data.paymentID || "",
+      subscriptionNo: data.subscriptionID || "",
+      paymentType: (data as any).paymentSource,
+    });
+  }
+
+  const handlePaypalOnCancel: PayPalButtonsComponentProps["onCancel"] = async (data) => {
+    await fetchUserOrderClose({
+      paymentChannel: PaymentChannel.Paypal,
+      paymentId: data.orderID as string,
+    })
+  }
+
+  const handlePaypalCreateSubscription: PayPalButtonsComponentProps["createSubscription"] = async () => {
+    const result = await fetchUserOrderCreate({
+      sku: skuInfo.bizId,
+      paymentChannel: PaymentChannel.Paypal,
+      paymentType: PaymentType.Card,
+      fbPixelId: searchParams.get('pixel') || '',
+      reback: `${location.pathname}${location.search || ''}`,
+    });
+    return result.subscriptionNo;
+  }
+
+  if (skuInfo.skuType === SkuType.Subscription) {
     return (
-        <div className="rounded-[16px] overflow-hidden [&_iframe]:!rounded-[16px]">
-            <PayPalButtons
-                style={styles}
-                createOrder={handlePaypalCreateOrder}
-                onApprove={handlePaypalOnApprove}
-                onCancel={handlePaypalOnCancel}
-            />
-        </div>
-    );
+      <div className="rounded-[16px] overflow-hidden [&_iframe]:!rounded-[16px]">
+        <PayPalButtons
+          style={styles}
+          createSubscription={handlePaypalCreateSubscription}
+          onApprove={handlePaypalOnApprove}
+          onCancel={handlePaypalOnCancel}
+        />
+      </div>
+    )
+  }
+
+  return (
+    <div className="rounded-[16px] overflow-hidden [&_iframe]:!rounded-[16px]">
+      <PayPalButtons
+        style={styles}
+        createOrder={handlePaypalCreateOrder}
+        onApprove={handlePaypalOnApprove}
+        onCancel={handlePaypalOnCancel}
+      />
+    </div>
+  );
 }
