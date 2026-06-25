db7a626dfee71407636f9c79b9e25a302fc986f1
hao <fortunellh@126.com>
2026-06-25 19:12:28 +0800

    update: tiktok

--- lib/repo/src/proxy/tiktok/business.ts
+++ lib/repo/src/proxy/tiktok/business.ts
@@ -1,30 +1,38 @@
 import { currentTime } from "@lib/common/utils/time";
 import type { TikTokBusinessEventReq } from "./business.interface";
+import { logger } from "@lib/internal/logger";
 
 class TikTokBusinessProxy {
     async sendEvent(req: TikTokBusinessEventReq) {
-        await fetch('https://business-api.tiktok.com/open_api/v1.3/event/track', {
+        logger.info(`sendEvent toiktok: ${JSON.stringify(req)}`);
+        const body = JSON.stringify({
+            event_source: 'web',
+            event_source_id: req.pixelCode,
+            data: [{
+                event: req.event,
+                event_time: currentTime(),
+                event_id: req.eventId,
+                user: {
+                    external_id: req.externalId,
+                },
+                page: {
+                    url: req.url,
+                }
+            }]
+        })
+        logger.info(`sendEvent toiktok body: ${JSON.stringify(body)}`);
+
+        const resp = await fetch('https://business-api.tiktok.com/open_api/v1.3/event/track', {
             method: 'POST',
             headers: {
                 'Authorization': req.accessToken,
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({
-                event_source: 'web',
-                event_source_id: req.pixelCode,
-                data: [{
-                    event: req.event,
-                    event_time: currentTime(),
-                    event_id: req.eventId,
-                    user: {
-                        external_id: req.externalId,
-                    },
-                    page: {
-                        url: req.url,
-                    }
-                }]
-            })
+            body: body,
         });
+
+        const data = await resp.json();
+        logger.info(`sendEvent toiktok: ${JSON.stringify(data)}`);
     }
 }
 
