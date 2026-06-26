7cec9743f18ffce63fefc8bda5708065a2d7602d
unknown <fortunellh@126.com>
2026-06-26 23:20:40 +0800

    fix: pix payment event

--- app/openapi/src/services/payermax/order/order-status-helper.ts
+++ app/openapi/src/services/payermax/order/order-status-helper.ts
@@ -2,6 +2,12 @@ import { OrderStatus } from "@lib/common/consts/order";
 import type { OrderSelect } from "@lib/repo/models/order";
 import { memberDelivery } from "@app/order/member";
 import { orderDao } from "@lib/repo/dao/order.dao";
+import { PaymentType } from "@lib/common/consts/payment";
+import { subscriptionService } from "../subscription-service";
+import { pixelDao } from "@lib/repo/dao/pixel.dao";
+import { subscriptionDao } from "@lib/repo/dao/subscription.dao";
+import { logger } from "@lib/internal/logger";
+import { PixelPlatform } from "@lib/common/consts/pixel";
 
 class OrderStatusHelper {
     async processChangeOrderStatus(orderInfo: OrderSelect, targetStatus: OrderStatus) {
@@ -13,6 +19,13 @@ class OrderStatusHelper {
                 this.processPaidShouldOrderStatus(orderInfo.id, orderInfo.orderStatus, targetStatus);
                 await memberDelivery.deliver(orderInfo);
                 await orderDao.updateOrderById(orderInfo.id, { orderStatus: OrderStatus.Completed });
+
+                const pixelInfo = await pixelDao.getPixelById(orderInfo.pixelId);
+                if (orderInfo.paymentType === PaymentType.Pix && pixelInfo.platfrom === PixelPlatform.Facebook) {
+                    logger.info(`OrderStatusHelper.processChangeOrderStatus: use pix logic - send event to fb`);
+                    const subsriptionInfo = await subscriptionDao.getSubscriptionById(orderInfo.subscriptionId);
+                    await subscriptionService.sendFacebookEvent(pixelInfo, subsriptionInfo);
+                }
                 break;
             default:
                 await orderDao.updateOrderById(orderInfo.id, { orderStatus: targetStatus });
