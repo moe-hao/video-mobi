2591a989222cbd37a257582c68e01febc3a02751
hao <fortunellh@126.com>
2026-07-02 15:01:57 +0800

    update: region

--- lib/common/src/consts/region.ts
+++ lib/common/src/consts/region.ts
@@ -3,6 +3,15 @@ export enum Region {
     BR = "BR",
     JP = "JP",
     TW = "TW",
+    CLP = "CLP",
+    COP = "COP",
+    MXN = "MXN",
+    PEN = "PEN",
+    PYG = "PYG",
+    ARS = "ARS",
+    USD = "USD",
+    UYU = "UYU",
+    CRC = "CRC",
 }
 
 export const RegionName: Record<Region, string> = {
@@ -10,6 +19,15 @@ export const RegionName: Record<Region, string> = {
     [Region.BR]: "巴西",
     [Region.JP]: "日本",
     [Region.TW]: "台湾",
+    [Region.CLP]: "智利",
+    [Region.COP]: "哥伦比亚",
+    [Region.MXN]: "墨西哥",
+    [Region.PEN]: "秘鲁",
+    [Region.PYG]: "危地马拉",
+    [Region.ARS]: "阿根廷",
+    [Region.USD]: "美元",
+    [Region.UYU]: "乌拉圭",
+    [Region.CRC]: "哥斯达黎加",
 }
 
 export enum Language {
