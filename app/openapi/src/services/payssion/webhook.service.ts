39d5bcc240d7850ae4d83309ea09b55f944e1a03
hao <fortunellh@126.com>
2026-07-10 12:35:38 +0800

    update: openapi

--- app/openapi/src/services/payssion/webhook.service.ts
+++ app/openapi/src/services/payssion/webhook.service.ts
@@ -7,6 +7,11 @@ import { pixelDao } from "@lib/repo/dao/pixel.dao";
 import { subscriptionDao } from "@lib/repo/dao/subscription.dao";
 import { subscriptionService } from "../payermax/subscription-service";
 import { payssionProxy } from "@lib/repo/proxy/payment/payssion";
+import { PaymentChannel, PaymentType } from "@lib/common/consts/payment";
+import { orderBizIdGenerator } from "@app/order/order/order-biz-id-generator";
+import { skuDao } from "@lib/repo/dao/sku.dao";
+import { productDao } from "@lib/repo/dao/product.dao";
+import { currentTime } from "@lib/common/utils/time";
 
 export class PayssionWebhookService {
     async handle(req: PayssionWebhookReq) {
@@ -31,27 +36,54 @@ export class PayssionWebhookService {
     private async handlePaymentSucceeded(req: PayssionWebhookReq<PayssionWebhookPaymentData>) {
         const subscriptionNo = req.data.object.source_id;
         const subscriptionInfo = await subscriptionDao.getSubscriptionByNo(subscriptionNo);
-        const [orderInfo] = await orderDao.getOrderListByUserIdAndSubscriptionId(subscriptionInfo.userId, subscriptionInfo.id);
-
-        await orderDao.updateOrderById(orderInfo.id, {
-            paymentId: req.data.object.id,
-            orderStatus: OrderStatus.Completed,
-        });
-
-        await MemberDeliveryFactory.create(orderInfo).deliver();
-
         const payssionSubscriptionInfo = await payssionProxy.getSubscriptionInfo(subscriptionNo);
-        if (payssionSubscriptionInfo.status === 'active') {
-            await subscriptionDao.updateSubscriptionById(subscriptionInfo.id, {
-                subscriptionStatus: SubscriptionStatus.Active,
+
+        let orderInfo = await orderDao.getOrderByPaymentIdAndChannel(req.data.object.id, PaymentChannel.Payssion);
+        if (!orderInfo) {
+            const skuInfo = await skuDao.getSkuById(subscriptionInfo.skuId);
+            const [productInfo] = await productDao.getProductListInIds([skuInfo.productId]);
+
+            const orderId = await orderDao.addOrder({
+                bizId: await orderBizIdGenerator.generate(),
+                userId: subscriptionInfo.userId,
+                amount: skuInfo.price,
+                currency: productInfo.currency,
+                skuId: skuInfo.id,
+                productId: productInfo.id,
+                pixelId: subscriptionInfo.pixelId,
+                paymentId: req.data.object.id,
+                subscriptionId: subscriptionInfo.id,
+                subscriptionCount: payssionSubscriptionInfo.times_completed,
+                paymentChannel: subscriptionInfo.subscriptionChannel,
+                paymentType: PaymentType.Pix,
+                orderType: skuInfo.skuType,
+                orderStatus: OrderStatus.Completed,
+                ad: '',
+                createTime: currentTime(),
+                updateTime: currentTime(),
             });
 
+            orderInfo = await orderDao.getOrderById(orderId);
+            await MemberDeliveryFactory.create(orderInfo).deliver();
+        } else {
             await orderDao.updateOrderById(orderInfo.id, {
-                subscriptionCount: payssionSubscriptionInfo.times_completed,
+                paymentId: req.data.object.id,
+                orderStatus: OrderStatus.Completed,
             });
+            await MemberDeliveryFactory.create(orderInfo).deliver();
+
+            const pixelInfo = await pixelDao.getPixelById(orderInfo.pixelId);
+            await subscriptionService.sendFacebookEvent(pixelInfo, subscriptionInfo);
+            if (payssionSubscriptionInfo.status === 'active') {
+                await subscriptionDao.updateSubscriptionById(subscriptionInfo.id, {
+                    subscriptionStatus: SubscriptionStatus.Active,
+                });
+
+                await orderDao.updateOrderById(orderInfo.id, {
+                    subscriptionCount: payssionSubscriptionInfo.times_completed,
+                });
+            }
         }
-        const pixelInfo = await pixelDao.getPixelById(orderInfo.pixelId);
-        await subscriptionService.sendFacebookEvent(pixelInfo, subscriptionInfo);
     }
 
     private async handlePaymentFailed(req: PayssionWebhookReq<PayssionWebhookPaymentData>) {
