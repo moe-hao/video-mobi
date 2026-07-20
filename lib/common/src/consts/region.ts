f7f72adc185f383cb6c8ab8329ff532b13b2e1ae
hao <fortunellh@126.com>
2026-07-20 16:52:16 +0800

    update: region

--- lib/common/src/consts/region.ts
+++ lib/common/src/consts/region.ts
@@ -12,80 +12,14 @@ export enum Region {
     PA = "PA",
     UY = "UY",
     CR = "CR",
+    ES = "ES",
+    DO = "DO",
 }
 
 export type RegionInfo = {
     name: string;
     currency: string;
     currencySign: string;
-}
-
-export const RegionInfoRecord: Record<Region, RegionInfo> = {
-    [Region.US]: {
-        name: "美国",
-        currency: "USD",
-        currencySign: "$",
-    },
-    [Region.BR]: {
-        name: "巴西",
-        currency: "BRL",
-        currencySign: "R$",
-    },
-    [Region.JP]: {
-        name: "日本",
-        currency: "JPY",
-        currencySign: "¥",
-    },
-    [Region.TW]: {
-        name: "台湾",
-        currency: "TWD",
-        currencySign: "NT$",
-    },
-    [Region.CL]: {
-        name: "智利",
-        currency: "CLP",
-        currencySign: "CLP$",
-    },
-    [Region.CO]: {
-        name: "哥伦比亚",
-        currency: "COP",
-        currencySign: "COP$",
-    },
-    [Region.MX]: {
-        name: "墨西哥",
-        currency: "MXN",
-        currencySign: "MXN$",
-    },
-    [Region.PE]: {
-        name: "秘鲁",
-        currency: "PEN",
-        currencySign: "S/",
-    },
-    [Region.PY]: {
-        name: "巴拉圭",
-        currency: "PYG",
-        currencySign: "₲",
-    },
-    [Region.AR]: {
-        name: "阿根廷",
-        currency: "ARS",
-        currencySign: "$",
-    },
-    [Region.PA]: {
-        name: "巴拿马",
-        currency: "PAB",
-        currencySign: "$",
-    },
-    [Region.UY]: {
-        name: "乌拉圭",
-        currency: "UYU",
-        currencySign: "$U",
-    },
-    [Region.CR]: {
-        name: "哥斯达黎加",
-        currency: "CRC",
-        currencySign: "$",
-    },
 }
 
 export const RegionName: Record<Region, string> = {
@@ -102,6 +36,8 @@ export const RegionName: Record<Region, string> = {
     [Region.PA]: "巴拿马",
     [Region.UY]: "乌拉圭",
     [Region.CR]: "哥斯达黎加",
+    [Region.ES]: "西班牙",
+    [Region.DO]: "多米尼加",
 }
 
 export enum Language {
