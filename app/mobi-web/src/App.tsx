2a25219f40fe6ed4488264172405fbb2a694bc97
hao <fortunellh@126.com>
2026-08-12 16:54:28 +0800

    update: mobi-web

--- app/mobi-web/src/App.tsx
+++ app/mobi-web/src/App.tsx
@@ -4,7 +4,7 @@ import { RouterProvider } from 'react-router';
 
 function App() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div className="flex flex-col flex-1 items-center justify-center bg-black font-sans">
       <main className="flex flex-1 w-full max-w-3xl flex-col sm:px-6 bg-white dark:bg-black">
         <ToastProvider>
           <RouterProvider router={router} />
