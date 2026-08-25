6f1a0266149046a881c05142faf49bd6cb4e2dc5
hao <fortunellh@126.com>
2026-08-25 16:46:57 +0800

    update: exchange

--- lib/repo/src/proxy/exchange/exchange.ts
+++ lib/repo/src/proxy/exchange/exchange.ts
@@ -1,7 +1,12 @@
+import http from "@lib/internal/http";
 import { logger } from "@lib/internal/logger";
 import { exchangeRedis } from "@lib/repo/redis/exchange";
 
 class ExchangeProxy {
+    constructor() {
+
+    }
+
     async getExchangeRate(base: string, target: string): Promise<number> {
         const cacheRateData = await exchangeRedis.getExchangeRate(base, target);
         if (cacheRateData) {
@@ -10,11 +15,10 @@ class ExchangeProxy {
         }
 
         logger.info(`ExchangeProxy.getExchangeRate: use api, ${base} -> ${target}`);
-        const resp = await fetch(`https://v6.exchangerate-api.com/v6/abfd0efcbdd12f2b49acd998/pair/${base}/${target}`);
-        logger.info(`ExchangeProxy.getExchangeRate: use api, ${base} -> ${target} response: ${await resp.text()}`);
-        const data = await resp.json();
-        exchangeRedis.setExchangeRate(base, target, data.conversion_rate);
-        return data.conversion_rate;
+        const result = await http.get<{ conversion_rate: number }>(`https://v6.exchangerate-api.com/v6/abfd0efcbdd12f2b49acd998/pair/${base}/${target}`);
+
+        await exchangeRedis.setExchangeRate(base, target, result.data.conversion_rate);
+        return result.data.conversion_rate;
     }
 }
 
