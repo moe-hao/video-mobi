33e7e7595f875933d4ea05c4fded230baf979599
hao <fortunellh@126.com>
2026-09-09 18:34:48 +0800

    update: useepay

--- app/openapi/src/services/useepay/event/event-handler.ts
+++ app/openapi/src/services/useepay/event/event-handler.ts
@@ -2,6 +2,8 @@ import { UseePayWebhookEventName } from "@lib/common/consts/useepay";
 import type { UseePayWebhookEvent } from "@lib/common/dto/useepay/useepay-event";
 import { PaymentIntentEventHandler } from "./payment-intent-event-handler";
 import { SubscriptionEventHandler } from "./subscription-event-handler";
+import { InternalException } from "@lib/common/exceptions/internal-exception";
+import { ResultCode } from "@lib/common/consts/result";
 
 export interface EventHandler {
     handle(event: UseePayWebhookEvent): Promise<void>;
@@ -14,5 +16,7 @@ export function createEventHandler(name: UseePayWebhookEventName): EventHandler 
             return new PaymentIntentEventHandler();
         case UseePayWebhookEventName.SubscriptionActive:
             return new SubscriptionEventHandler();
+        default:
+            throw new InternalException(ResultCode.MethodNotSupported);
     }
 }
--- app/openapi/src/services/useepay/webhook.service.ts
+++ app/openapi/src/services/useepay/webhook.service.ts
@@ -1,11 +1,17 @@
 import type { UseePayWebhookEvent } from "@lib/common/dto/useepay/useepay-event";
 import { createEventHandler } from "./event/event-handler";
 import { logger } from "@lib/internal/logger";
+import { InternalException } from "@lib/common/exceptions/internal-exception";
+import { ResultCode } from "@lib/common/consts/result";
 
 export async function handleWebhookEvent(req: UseePayWebhookEvent) {
     try {
         await createEventHandler(req.name).handle(req);
     } catch (error) {
-        logger.error(error);
+        if (error instanceof InternalException && error.code === ResultCode.MethodNotSupported.code) {
+            logger.info(`Method ${req.name} is not supported`);
+        } else {
+            logger.error(error);
+        }
     }
 }
