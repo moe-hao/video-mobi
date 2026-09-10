419855489d3fcf32d8166e0f08ff9bfdd0dae5c5
hao <fortunellh@126.com>
2026-09-10 17:07:19 +0800

    update: useepay

--- app/mobi/src/services/order/payment/useepay-payment.ts
+++ app/mobi/src/services/order/payment/useepay-payment.ts
@@ -1,4 +1,4 @@
-import { PaymentChannel, PaymentTypeToUseePayPaymentMethod } from "@lib/common/consts/payment";
+import { PaymentChannel, PaymentType, PaymentTypeToUseePayPaymentMethod, UseePayPaymentMethod } from "@lib/common/consts/payment";
 import type { Payment, PaymentInfo, PaymentOrder } from "./payment";
 import { SkuType } from "@lib/common/consts/sku";
 import { SubscriptionStatus, type PeriodType } from "@lib/common/consts/subscription";
@@ -63,7 +63,6 @@ export class UseePayPayment implements Payment {
 
     async createPaymentIntent(invoiceId: string, subscriptionId: number, subscriptionNo: string, paymentInfo: PaymentInfo): Promise<PaymentOrder> {
         const orderBizId = await orderBizIdGenerator.generate();
-        const paymentMethodType = PaymentTypeToUseePayPaymentMethod.get(paymentInfo.paymentType);
         const useePayPaymentInfo = await useePayProxy.createPaymentIntent({
             amount: paymentInfo.skuInfo.price,
             currency: paymentInfo.skuInfo.currency,
@@ -73,7 +72,7 @@ export class UseePayPayment implements Payment {
             invoiceId: invoiceId,
             subscriptionId: subscriptionNo,
             returnUrl: `http://${paymentInfo.productInfo.host}${paymentInfo.reback}`,
-            paymentMethodTypes: paymentMethodType ? [paymentMethodType] : undefined,
+            paymentMethodTypes: this.getPaymentMethodType(paymentInfo)
         });
 
         const collectionBizId = (() => { try { return JSON.parse(paymentInfo.ad || "{}").collectionId || ""; } catch { return ""; } })();
@@ -102,5 +101,26 @@ export class UseePayPayment implements Payment {
             paymentId: useePayPaymentInfo.id,
             redirectUrl: useePayPaymentInfo.nextAction?.redirect?.url || "",
         }
+    }
+
+    private getPaymentMethodType(paymentInfo: PaymentInfo): UseePayPaymentMethod[] | undefined {
+        if (paymentInfo.paymentType === PaymentType.Card) {
+            return [UseePayPaymentMethod.Card];
+        }
+
+        if (paymentInfo.paymentType === PaymentType.Pix) {
+            return [UseePayPaymentMethod.Pix];
+        }
+
+        if (paymentInfo.paymentType === PaymentType.GooglePay) {
+            return [UseePayPaymentMethod.GooglePay];
+        }
+
+        if (paymentInfo.paymentType === PaymentType.ApplePay) {
+            return [UseePayPaymentMethod.ApplePay, UseePayPaymentMethod.GooglePay];
+        }
+
+        const paymentMethodType = PaymentTypeToUseePayPaymentMethod.get(paymentInfo.paymentType);
+        return paymentMethodType ? [paymentMethodType] : undefined;
     }
 }
